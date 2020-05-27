8a4f0870c92faac07fcbb7ee88a0400926d20751
ars <rhedikov@micro-solution.ru>
2020-05-27 16:36:11 +0300

    Add Calendar Class

---
+++ BPA/Model/ProductCalendar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPA.Model {
+    /// <summary>
+    /// Справочник продуктовых календарей
+    /// </summary>
+    class ProductCalendar : TableBase {
+        public override string TableName => "Продуктовые_календари";
+        public override string SheetName => "Продуктовые календари";
+
+        public override IDictionary<string, string> Filds {
+            get {
+                return _filds;
+            }
+        }
+        private readonly Dictionary<string, string> _filds = new Dictionary<string, string>
+        {
+            { "Id", "№" },
+            { "Name", "Название" },
+            { "Path", "Путь к файлу" },
+        };
+
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int Id {
+            get; set;
+        }
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name {
+            get; set;
+        }
+        /// <summary>
+        /// Путь к файлу
+        /// </summary>
+        public string Path {
+            get; set;
+        }
+
+
+    }
+}
