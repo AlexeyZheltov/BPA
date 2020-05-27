30d37cf38ecb70c61418a9f69b62aad32e671cfa
ars <rhedikov@micro-solution.ru>
2020-05-27 16:51:36 +0300

    Add RRC Class

---
+++ BPA/Model/RRC.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPA.Model {
+    /// <summary>
+    /// Справочник РРЦ
+    /// </summary>
+    class RRC : TableBase {
+        public override string TableName => "РРЦ";
+        public override string SheetName => "РРЦ";
+
+        public override IDictionary<string, string> Filds {
+            get {
+                return _filds;
+            }
+        }
+        private readonly Dictionary<string, string> _filds = new Dictionary<string, string>
+        {
+            { "Article", "Артикул" },
+            { "IRP", "IRP, Eur" },
+            { "RRCNDS", "РРЦ, руб. с НДС" },
+            { "DIY", "DIY price list, руб. без НДС" },
+            { "Date", "Дата принятия" }
+        };
+
+        /// <summary>
+        /// Артикул
+        /// </summary>
+        public int Article {
+            get; set;
+        }
+        /// <summary>
+        /// IRP, Eur
+        /// </summary>
+        public string IRP {
+            get; set;
+        }
+
+        /// <summary>
+        /// РРЦ, руб. с НДС
+        /// </summary>
+        public string RRCNDS {
+            get; set;
+        }
+
+        /// <summary>
+        /// DIY price list, руб. без НДС
+        /// </summary>
+        public string DIY {
+            get; set;
+        }
+
+        /// <summary>
+        /// Дата принятия
+        /// </summary>
+        public string Date {
+            get; set;
+        }
+
+    }
+}
