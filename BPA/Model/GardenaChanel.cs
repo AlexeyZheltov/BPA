05eb5176d1d49e3f93b1ca18b14ac078356e2a06
ars <rhedikov@micro-solution.ru>
2020-05-27 17:41:22 +0300

    Add GardenaChanel Class

---
+++ BPA/Model/GardenaChanel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPA.Model {
+    /// <summary>
+    /// Справочник клиентов
+    /// </summary>
+    class GardenaChannel : TableBase {
+        public override string TableName => "Клиенты";
+        public override string SheetName => "Клиенты";
+
+        public override IDictionary<string, string> Filds {
+            get {
+                return _filds;
+            }
+        }
+        private readonly Dictionary<string, string> _filds = new Dictionary<string, string>
+        {
+            { "Id", "Код" },
+            { "GardenaChannel", "Gardena_Channel" },
+            { "SalesManager", "Sales manager" }
+        };
+
+        /// <summary>
+        /// Код
+        /// </summary>
+        public int Id {
+            get; set;
+        }
+
+        /// <summary>
+        /// GardenaChannel
+        /// </summary>
+        public int Gardena_Channel {
+            get; set;
+        }
+
+        /// <summary>
+        /// Sales manager
+        /// </summary>
+        public string SalesManager {
+            get; set;
+        }
+    }
+}
