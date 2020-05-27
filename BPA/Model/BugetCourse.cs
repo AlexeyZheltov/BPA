b14ab41fc07e48afef5fa079e5b97d7e7870760b
ars <rhedikov@micro-solution.ru>
2020-05-27 17:12:09 +0300

    Add budgetCourse Class

---
+++ BPA/Model/BugetCourse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPA.Model {
+    /// <summary>
+    /// Справочник бюджетных курсов
+    /// </summary>
+    class BugetCourse : TableBase {
+        public override string TableName => "Бюджетные_курсы";
+        public override string SheetName => "Бюджетные курсы";
+
+        public override IDictionary<string, string> Filds {
+            get {
+                return _filds;
+            }
+        }
+        private readonly Dictionary<string, string> _filds = new Dictionary<string, string>
+        {
+            { "Id", "№" },
+            { "Date", "Дата принятия" },
+            { "RRCPercent", "Процент от РРЦ, без НДС" }
+        };
+
+        /// <summary>
+        /// №
+        /// </summary>
+        public int Id {
+            get; set;
+        }
+   
+        /// <summary>
+        /// Дата принятия
+        /// </summary>
+        public string Date {
+            get; set;
+        }
+
+        /// <summary>
+        /// Процент от РРЦ, без НДС
+        /// </summary>
+        public string RRCPercent {
+            get; set;
+        }
+
+    }
+}
