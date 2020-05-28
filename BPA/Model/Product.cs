4a3a2b43b5ae72701438fe05d903c8c262dee514
ars <rhedikov@micro-solution.ru>
2020-05-28 17:19:27 +0300

    add calendar columns to Product class

--- BPA/Model/Product.cs
+++ BPA/Model/Product.cs
@@ -16,28 +16,56 @@ namespace BPA.Model {
         public override IDictionary<string, string> Filds => _filds;
         private readonly Dictionary<string, string> _filds = new Dictionary<string, string>
         {
-            { "Категория для прайс-листа диллеров", "Category" },
-            { "Суперкатегория(ENG)", "SupercategoryEng" },
-            { "Суперкатегория(RUS)", "SupercategoryRu" },
-            { "Продукт группа", "ProductGroup" },
-            { "Название продукт группы(ENG)", "ProductGroupEng" },
-            { "Название продукт группы(RUS)", "ProductGroupRu" },
+            { "Id","№" },
+            { "Category","Категория для прайс-листа диллеров" },
+            { "SupercategoryEng","Суперкатегория(ENG)"  },
+            { "SupercategoryRu","Суперкатегория(RUS)" },
+            { "ProductGroup","Продукт группа" },
+            { "ProductGroupEng","Название продукт группы(ENG)" },
+            { "ProductGroupRu","Название продукт группы(RUS)" },
             { "SubGroup", "SubGroup" },
-            { "Generic Name(long)", "GenericName" },
+            { "GenericName", "Generic Name(long)" },
             { "Model", "Model" },
             { "PNS", "PNS" },
-            { "Артикул", "Article" },
-            { "Артикул предшественника(если есть)", "ArticleOld" },
-            { "Название артикула(ENG)", "ArticleEng" },
-            { "Название артикула(RUS)", "ArticleRu" },
-            { "Используемый календарь", "Calendar" },
-            { "Статус", "Status" },
-            { "Эксклюзив клиента или канала продажи", "Exclusive" },
-            { "Локальный сертификат", "LocalCertificate" }
+            { "Article","Артикул" },
+            { "ArticleOld","Артикул предшественника(если есть)" },
+            { "ArticleEng","Название артикула(ENG)" },
+            { "ArticleRu","Название артикула(RUS)" },
+            { "Calendar", "Используемый календарь" },
+
+            { "CalendarToBeSoldIn","to be sold in" },
+            { "CalendarSalesStartDate","Sales Start Date" },
+            { "CalendarPreliminaryEliminationDate","Preliminary Elimination Date" },
+            { "CalendarEliminationDate","Elimination Date" },
+            { "CalendarGTIN","GTIN-13/EAN" },
+            { "CalendarCurrentProducingFactoryEntityReference","Current Producing Factory Entity Reference" },
+            { "CalendarCountryOfOrigin","Country of Origin" },
+            { "CalendarUnitOfMeasure","Unit of measure" },
+            { "CalendarQuantityInMasterPack","Quantity in Master pack" },
+            { "CalendarArticleGrossWeightPreliminary","Article gross weight, preliminary" },
+            { "CalendarArticleGrossWeight","Article gross weight" },
+            { "CalendarArticleNetWeightPreliminary","Article net weight, preliminary" },
+            { "CalendarArticleNetWeight","Article net weight" },
+            { "CalendarPackagingLength","Packaging length" },
+            { "CalendarPackagingWidth","Packaging width" },
+            { "CalendarPackagingHeight","Packaging height" },
+            { "CalendarPackagingVolume","Packaging volume" },
+            { "CalendarProductSizeLength","Product size length" },
+            { "CalendarProductSizeHeight","Product size height" },
+            { "CalendarProductSizeWidth","Product size width" },
+            { "CalendarUnitsPerPallet","Units Per Pallet" },
+
+            { "Status","Статус" },
+            { "Exclusive","Эксклюзив клиента или канала продажи" },
+            { "LocalCertificate","Локальный сертификат" }
         };
 
 
-
+        #region main
+        /// <summary>
+        /// №
+        /// </summary>
+        public string Id { get; set; }
         /// <summary>
         /// Категория для прайс-листа диллеров
         /// </summary>
@@ -45,110 +73,243 @@ namespace BPA.Model {
         /// <summary>
         /// Суперкатегория(ENG)
         /// </summary>
-        public string SupercategoryEng {
-            get; set;
-        }
+        public string SupercategoryEng { get; set; }
         /// <summary>
         /// Суперкатегория(RUS)
         /// </summary>
-        public string SupercategoryRu {
-            get; set;
-        }
+        public string SupercategoryRu { get; set; }
         /// <summary>
         /// Продукт группа
         /// </summary>
-        public string ProductGroup {
-            get; set;
-        }
+        public string ProductGroup { get; set; }
         /// <summary>
         /// Название продукт группы(ENG)
         /// </summary>
-        public string ProductGroupEng {
-            get; set;
-        }
+        public string ProductGroupEng { get; set; }
         /// <summary>
         /// Название продукт группы(RUS)
         /// </summary>
-        public string ProductGroupRu {
-            get; set;
-        }
+        public string ProductGroupRu { get; set; }
         /// <summary>
         /// SubGroup
         /// </summary>
-        public string SubGroup {
-            get; set;
-        }
+        public string SubGroup { get; set; }
         /// <summary>
         /// Generic Name(long)
         /// </summary>
-        public string GenericName {
-            get; set;
-        }
+        public string GenericName { get; set; }
         /// <summary>
         /// Model
         /// </summary>
-        public string Model {
-            get; set;
-        }
+        public string Model { get; set; }
         /// <summary>
         /// PNS
         /// </summary>
-        public string PNS {
-            get; set;
-        }
+        public string PNS { get; set; }
         /// <summary>
         /// Артикул
         /// </summary>
-        public string Article {
-            get; set;
-        }
+        public string Article { get; set; }
         /// <summary>
         /// Артикул предшественника(если есть)
         /// </summary>
-        public string ArticleOld {
-            get; set;
-        }
+        public string ArticleOld { get; set; }
         /// <summary>
         /// Название артикула(ENG)
         /// </summary>
-        public string ArticleEng {
-            get; set;
-        }
+        public string ArticleEng { get; set; }
         /// <summary>
         /// Название артикула(RUS)
         /// </summary>
-        public string ArticleRu {
-            get; set;
-        }
+        public string ArticleRu { get; set; }
         /// <summary>
         /// Используемый календарь
         /// </summary>
-        public string Calendar {
-            get; set;
-        }
+        public string Calendar { get; set; }
         /// <summary>
         /// Статус
         /// </summary>
-        public string Status {
-            get; set;
-        }
+        public string Status { get; set; }
         /// <summary>
         /// Эксклюзив клиента или канала продажи
         /// </summary>
-        public string Exclusive {
-            get; set;
-        }
+        public string Exclusive { get; set; }
         /// <summary>
         /// Локальный сертификат
         /// </summary>
-        public string LocalCertificate {
+        public string LocalCertificate { get; set;
+        }
+        #endregion
+
+        #region From Calendar
+
+        /// <summary>
+        /// to be sold in
+        /// </summary>
+        public string CalendarToBeSoldIn { get; set; }
+
+        /// <summary>
+        /// Sales Start Date
+        /// </summary>
+        public string CalendarSalesStartDate
+        {
             get; set;
         }
 
+        /// <summary>
+        /// Preliminary Elimination Date
+        /// </summary>
+        public string CalendarPreliminaryEliminationDate
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarEliminationDate
+        /// </summary>
+        public string CalendarEliminationDate
+        {
+            get; set;
+        }
+        /// <summary>
+         /// CalendarGTIN
+         /// </summary>
+        public string CalendarGTIN
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarCurrentProducingFactoryEntityReference
+        /// </summary>
+        public string CalendarCurrentProducingFactoryEntityReference
+        {
+            get; set;
+        }
+        /// <summary>
+         /// CalendarCountryOfOrigin
+         /// </summary>
+        public string CalendarCountryOfOrigin
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarUnitOfMeasure
+        /// </summary>
+        public string CalendarUnitOfMeasure
+        {
+            get; set;
+        }
+        /// <summary>
+        /// CalendarQuantityInMasterPack
+        /// </summary>
+        public string CalendarQuantityInMasterPack
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarArticleGrossWeightPreliminary
+        /// </summary>
+        public string CalendarArticleGrossWeightPreliminary
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarArticleGrossWeight
+        /// </summary>
+        public string CalendarArticleGrossWeight
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarArticleNetWeightPreliminary
+        /// </summary>
+        public string CalendarArticleNetWeightPreliminary
+        {
+            get; set;
+        }
+
+        /// <summary>
+        ///CalendarArticleNetWeight
+        /// </summary>
+        public string CalendarArticleNetWeight
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarPackagingLength
+        /// </summary>
+        public string CalendarPackagingLength
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarPackagingWidth
+        /// </summary>
+        public string CalendarPackagingWidth
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarPackagingHeight
+        /// </summary>
+        public string CalendarPackagingHeight
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarPackagingVolume
+        /// </summary>
+        public string CalendarPackagingVolume
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarProductSizeLength
+        /// </summary>
+        public string CalendarProductSizeLength
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarProductSizeHeight
+        /// </summary>
+        public string CalendarProductSizeHeight
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarProductSizeWidth
+        /// </summary>
+        public string CalendarProductSizeWidth
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// CalendarUnitsPerPallet
+        /// </summary>
+        public string CalendarUnitsPerPallet
+        {
+            get; set;
+        }
+
+        #endregion
+        
         public Product GetProduct(string articul)
         {
 
-            
             ListRow listRow = GetRow("Article", articul);
             if (listRow != null)
             {
