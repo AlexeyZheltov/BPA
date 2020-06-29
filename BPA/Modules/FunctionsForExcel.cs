fb63e6e680e4fece87af26f05fabb6d4e3f37059
ars <rhedikov@micro-solution.ru>
2020-06-29 16:05:52 +0300

    Add function for copy sheet

--- BPA/Modules/FunctionsForExcel.cs
+++ BPA/Modules/FunctionsForExcel.cs
@@ -1,5 +1,6 @@
 ﻿using Excel = Microsoft.Office.Interop.Excel;
 using System.Collections.Generic;
+using Microsoft.Office.Interop.Excel;
 
 namespace BPA.Modules
 {
@@ -79,6 +80,49 @@ namespace BPA.Modules
             catch { return false; }
         }
 
+        /// <summary>
+        /// Создает и возвращает копию листа в текщей книге с указанным именем или именем по умолчанию
+        /// </summary>
+        /// <param name="worksheet"></param>
+        /// <param name="worksheetName"></param>
+        /// <returns></returns>
+        public static Excel.Worksheet CreateSheetCopy(Excel.Worksheet worksheet, string copyWorksheetName = "", string afterSheetName = "")
+        {
+            Excel.Worksheet afterSheet = (afterSheetName != null && IsSheetExists(afterSheetName)) ? worksheet.Parent.Sheets[afterSheetName] : worksheet;
+
+            worksheet.Copy(After: afterSheet);
+            Excel.Worksheet newWorksheet = Application.ActiveSheet;
+
+            newWorksheet.Name = nextNumSheet(copyWorksheetName);
+
+            return newWorksheet;
+        }
+
+        /// <summary>
+        /// проверка наличия листа с текущим именем
+        /// </summary>
+        /// <param name="sheetName"></param>
+        /// <returns></returns>
+        private static string nextNumSheet(string sheetName)
+        {
+            if (!IsSheetExists(sheetName))
+                return sheetName;
+
+            if (double.TryParse(sheetName.Substring(sheetName.Length - 1, 1), out double strNum))
+            {
+                strNum++;
+                sheetName = sheetName.Substring(0, sheetName.Length - 2);
+            }
+            else
+            {
+                strNum = 1;
+            }
+
+            sheetName=sheetName + "_" + strNum.ToString();                    
+            return nextNumSheet(sheetName);
+        }
+
+
         public static void HideShowSettingsSheets()
         {
             List<string> AlwaysShowSheets = new List<string>
@@ -103,6 +147,30 @@ namespace BPA.Modules
             foreach (Excel.Worksheet sheet in Globals.ThisWorkbook.Sheets)
                 if (!AlwaysShowSheets.Contains(sheet.Name))
                     sheet.Visible = status;
+        }
+
+        public static void ShowSheet(string sheetName)
+        {
+            if (!IsSheetExists(sheetName))
+                return;
+
+            Worksheet worksheet = Globals.ThisWorkbook.Sheets[sheetName];
+            if (worksheet.Visible == XlSheetVisibility.xlSheetHidden)
+            {
+                worksheet.Visible = XlSheetVisibility.xlSheetVisible;
+            }
+        }
+
+        public static void HideSheet(string sheetName)
+        {
+            if (!IsSheetExists(sheetName))
+                return;
+
+            Worksheet worksheet = Globals.ThisWorkbook.Sheets[sheetName];
+            if (worksheet.Visible == XlSheetVisibility.xlSheetVisible)
+            {
+                worksheet.Visible = XlSheetVisibility.xlSheetHidden;
+            }
         }
     }
 }
