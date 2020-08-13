8d651ddb271e52e9beb4ad71db73d8f8a6ceae00
arslan21 <r.shedikov@mail.ru>
2020-08-13 17:29:42 +0300

    Add Model/ProductCalendar

---
+++ BPA/NewModel/ProductCalendarItem.cs
@@ -0,0 +1,47 @@
+﻿using BPA.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPA.NewModel
+{
+    class ProductCalendarItem
+    {
+        TableRow _row;
+        public ProductCalendarItem(TableRow row) => _row = row;
+
+        #region Основные свойства
+
+        public int Id
+        {
+            get => _row["№"];
+            set => _row["№"] = value;
+        }
+
+        public string Name
+        {
+            get => _row["Название"];
+            set => _row["Название"] = value;
+        }
+
+        public string Path
+        {
+            get => _row["Путь к файлу"];
+            set => _row["Путь к файлу"] = value;
+        }
+        #endregion
+
+        public void UpdateFromCalendar(FileCalendar fileCalendar) 
+        {
+            Name = fileCalendar.Workbook.Name;
+            Path = fileCalendar.Workbook.Path;
+        }
+
+        public void UpdateProductsFromCalendar(List<FileCalendar.ProductFromCalendar> productsFrom)
+        {
+
+        }
+    }
+}
