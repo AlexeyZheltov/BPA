0213f4ce91c4539ba26823dfd6c367d431a154d1
arslan21 <r.shedikov@mail.ru>
2020-08-26 17:17:39 +0300

    Add WaitForm

---
+++ BPA/Forms/WaitForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace BPA.Forms
+{
+    partial class WaitForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textLabel = new System.Windows.Forms.Label();
+            this.textTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textLabel
+            // 
+            this.textLabel.AutoSize = true;
+            this.textLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textLabel.Location = new System.Drawing.Point(12, 9);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(115, 24);
+            this.textLabel.TabIndex = 0;
+            this.textLabel.Text = "Подождите";
+            // 
+            // textTimer
+            // 
+            this.textTimer.Enabled = true;
+            this.textTimer.Interval = 250;
+            this.textTimer.Tick += new System.EventHandler(this.textTimer_Tick);
+            // 
+            // WaitForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 45);
+            this.Controls.Add(this.textLabel);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WaitForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BPA";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.WaitForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label textLabel;
+        private System.Windows.Forms.Timer textTimer;
+    }
+}
---
+++ BPA/Forms/WaitForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BPA.Forms
+{
+    public partial class WaitForm : Form
+    {
+        string text;
+        public WaitForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textTimer_Tick(object sender, EventArgs e)
+        {
+            textLabel.Text = textLabel.Text.Length < text.Length + 4 ? $"{textLabel.Text}." : text;
+        }
+
+        private void WaitForm_Load(object sender, EventArgs e)
+        {
+            text = textLabel.Text;
+        }
+    }
+}
